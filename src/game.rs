c429e2808239b145a99eed469bc59f2f571af21e
Di Saber <marc.a.benamou@gmail.com>
2024-01-09 09:53:33 -0600

    Disable alpha blending for debugging

--- src/game.rs
+++ src/game.rs
@@ -20,8 +20,8 @@ impl Game {
         gl::load_with(|s| window.get_proc_address(s));
 
         unsafe {
-            gl::Enable(gl::BLEND);
-            gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            //gl::Enable(gl::BLEND);
+            //gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
         }
 
         Self { glfw, window }
